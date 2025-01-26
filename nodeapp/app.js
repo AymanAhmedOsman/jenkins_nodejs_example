09a5637cd77502b9691a9396d6a508ae3708bf28
Ayman Osman <95719911+AymanAhmedOsman@users.noreply.github.com>
2025-01-26 13:34:48 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from form IT visionary')
+    res.send('Hello Ayman from form IT visionary')
   })
   
   app.listen(port, () => {
